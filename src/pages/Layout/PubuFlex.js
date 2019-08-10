b23b86588fde43be8bc392d0ea47a7137b928f6e
jiangxia <850304188@qq.com>
2019-08-10 19:44:45 +0800

    瀑布流-flex布局上传

--- src/pages/Layout/PubuFlex.js
+++ src/pages/Layout/PubuFlex.js
@@ -1,10 +1,48 @@
 import React, { Component } from 'react'
+import  './PubuFlex.less'
 
 export default class PubuFlex extends Component {
   render() {
     return (
       <div>
-        1
+        <div className='g-container'>
+          <div className="g-queue">
+            <div className="g-item"></div>
+            <div className="g-item"></div>
+            <div className="g-item"></div>
+            <div className="g-item"></div>
+            <div className="g-item"></div>
+            <div className="g-item"></div>
+            <div className="g-item"></div>
+          </div>
+          <div className="g-queue">
+            <div className="g-item"></div>
+            <div className="g-item"></div>
+            <div className="g-item"></div>
+            <div className="g-item"></div>
+            <div className="g-item"></div>
+            <div className="g-item"></div>
+            <div className="g-item"></div>
+          </div>
+          <div className="g-queue">
+            <div className="g-item"></div>
+            <div className="g-item"></div>
+            <div className="g-item"></div>
+            <div className="g-item"></div>
+            <div className="g-item"></div>
+            <div className="g-item"></div>
+            <div className="g-item"></div>
+          </div>
+          <div className="g-queue">
+            <div className="g-item"></div>
+            <div className="g-item"></div>
+            <div className="g-item"></div>
+            <div className="g-item"></div>
+            <div className="g-item"></div>
+            <div className="g-item"></div>
+            <div className="g-item"></div>
+          </div>
+        </div>
       </div>
     )
   }
